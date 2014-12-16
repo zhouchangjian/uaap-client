8c7d14b5a22217eb8587778fb0b1493e8b615354
zhouchangjiang <1318877260@qq.com>
2014-12-16 17:59:43 +0800

    update

--- src/main/java/com/cj/uaap/oauth2/OAuth2AuthenticationFilter.java
+++ src/main/java/com/cj/uaap/oauth2/OAuth2AuthenticationFilter.java
@@ -5,22 +5,17 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.AccessControlFilter;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
-import org.apache.shiro.web.filter.authc.AuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
-
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-
-/**
- * <p>User: Zhang Kaitao
- * <p>Date: 14-2-18
- * <p>Version: 1.0
- */
 public class OAuth2AuthenticationFilter extends AuthenticatingFilter {
 
+	Logger loger = LoggerFactory.getLogger(OAuth2AuthenticationFilter.class);
     //oauth2 authc code参数名
     private String authcCodeParam = "code";
     //客户端id
@@ -54,37 +49,43 @@ public class OAuth2AuthenticationFilter extends AuthenticatingFilter {
 
     @Override
     protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+    	loger.info("createToken：开始");
+    	HttpServletRequest httpRequest = (HttpServletRequest) request;
         String code = httpRequest.getParameter(authcCodeParam);
+        loger.info("createToken：code="+code);
         return new OAuth2Token(code);
     }
 
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
-        return false;
+    	loger.info("isAccessAllowed：开始");
+    	return false;
     }
 
     @Override
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-
-    	System.out.println("#####################过滤器开始拦截#########################");
+    	loger.info("onAccessDenied：开始");
         String error = request.getParameter("error");
+        loger.info("onAccessDenied：error="+error);
         String errorDescription = request.getParameter("error_description");
+        loger.info("onAccessDenied：errorDescription="+errorDescription);
         if(!StringUtils.isEmpty(error)) {//如果服务端返回了错误
+        	loger.info("onAccessDenied：error="+error);
             WebUtils.issueRedirect(request, response, failureUrl + "?error=" + error + "error_description=" + errorDescription);
             return false;
         }
 
         Subject subject = getSubject(request, response);
-        System.out.println(subject.isAuthenticated());
-        System.out.println(StringUtils.isEmpty(request.getParameter(authcCodeParam)));
         if(StringUtils.isEmpty(request.getParameter(authcCodeParam))){
+        	loger.info("onAccessDenied：authcCodeParam="+request.getParameter(authcCodeParam));
         	 if(subject.isAuthenticated()){
+        		 loger.info("onAccessDenied：isAuthenticated="+subject.isAuthenticated());
         		 return true;
         	 }
         	 saveRequestAndRedirectToLogin(request, response);
              return false;
         }else{
+        	 loger.info("onAccessDenied：executeLogin开始");
         	 return executeLogin(request, response);
         }
 
@@ -93,13 +94,16 @@ public class OAuth2AuthenticationFilter extends AuthenticatingFilter {
     @Override
     protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request,
                                      ServletResponse response) throws Exception {
+    	loger.info("onLoginSuccess：issueSuccessRedirect：开始");
         issueSuccessRedirect(request, response);
+        loger.info("onLoginSuccess：issueSuccessRedirect：结束");
         return false;
     }
 
     @Override
     protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException ae, ServletRequest request,
                                      ServletResponse response) {
+    	loger.info("onLoginFailure：开始");
         Subject subject = getSubject(request, response);
         if (subject.isAuthenticated() || subject.isRemembered()) {
             try {
